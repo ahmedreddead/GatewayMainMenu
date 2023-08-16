16d6603753941bcc6c531b161dac81ce25990315
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2023-08-16 19:19:41 +0400

    14/08/2023 after graphs

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -1,10 +1,9 @@
-// Function to map status values to colors
-// Initialize Flatpickr
+
 flatpickr("#start-time", {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-    time_24hr: true
-});
+        enableTime: true,
+        dateFormat: "Y-m-d H:i",
+        time_24hr: true
+    });
 
 flatpickr("#end-time", {
     enableTime: true,
@@ -15,24 +14,25 @@ flatpickr("#end-time", {
 function mapStatusToValue(status) {
     switch (status) {
         case 'on':
-            return 1;
+            return 10;
         case 'off':
-            return 0;
+            return 1;
         case 'Motion Detected':
-            return 1;
+            return 10;
         case 'No Motion':
-            return 0;
-        case 'opened':
             return 1;
+        case 'opened':
+            return 10;
         case 'closed':
-            return 0;
+            return 1;
+        case 'No Glass Break ':
+            return 1;
+
         // Add more cases for other status values
         default:
             return 0 ; // Use black for unknown statuses
     }
 }
-
-// Function to fetch data using jQuery AJAX and draw graphs
 function fetchAndDrawGraph() {
     var startTime = document.getElementById('start-time').value;
     var endTime = document.getElementById('end-time').value;
@@ -47,20 +47,57 @@ function fetchAndDrawGraph() {
             allData.forEach(itemData => {
                 if (itemData.data.length > 0) {
                     var graphContainer = document.createElement('div');
+                    graphContainer.className = 'graph-container';
                     graphContainer.id = `graph-${itemData.item.itemId}`;
                     document.getElementById('graph-container').appendChild(graphContainer);
 
-                    var graphData = {
-                        x: itemData.data.map(item => new Date(item[2])),
-                        y: itemData.data.map(item => mapStatusToValue(item[1])),
-                        type: 'bar'
-                    };
+                    var graphData = [];
+
+                    var currentStatus = -1; // Initialize to an invalid value
+                    var startTime = null;
+                    var endTime = null;
+                    var statusLabel = ''; // Initialize status label
+
+                    itemData.data.forEach(item => {
+                        var timestamp = new Date(item[2]);
+                        var statusValue = mapStatusToValue(item[1]);
+
+                        if (statusValue !== currentStatus) {
+                            if (currentStatus === 1) {
+                                // End of an "on" interval
+                                endTime = timestamp;
+                            } else {
+                                // Start of an "on" interval
+                                startTime = timestamp;
+                                statusLabel = getStatusLabel(itemData.item.type, statusValue); // Get status label based on sensor type
+                            }
+
+                            // Create a single bar for the interval
+                            graphData.push({
+                                x: [startTime, endTime],
+                                y: [1, 1],
+                                type: 'bar',
+                                opacity: 0.6, // Adjust the opacity as needed
+                                hoverinfo: 'none', // Disable hover info for clarity
+                                name: statusLabel, // Display customized status label
+                                showlegend: false, // Hide legend entry
+                            });
+
+                            currentStatus = statusValue;
+                        }
+                    });
+
                     var layout = {
                         title: `${itemData.item.type} - ${itemData.item.itemId}`,
                         xaxis: {title: 'Time'},
-                        yaxis: {title: 'Status'}
+                        yaxis: {title: 'Status'},
+                        barmode: 'overlay', // Overlay bars instead of stacking,
+                        annotations: [] ,// Clear annotations
+                        width: 600, // Adjust the width of the graph
+                        height: 400, // Adjust the height of the graph
                     };
-                    Plotly.newPlot(graphContainer.id, [graphData], layout);
+
+                    Plotly.newPlot(graphContainer.id, graphData, layout, {responsive: true, displayModeBar: false});
                 }
             });
         },
@@ -69,6 +106,16 @@ function fetchAndDrawGraph() {
         }
     });
 
+}
+
+// Function to get status label based on sensor type
+function getStatusLabel(sensorType, statusValue) {
+    if (sensorType === 'door') {
+        return statusValue === 1 ? 'Opened' : 'Closed';
+    } else if (sensorType === 'motion') {
+        return statusValue === 1 ? 'Motion Detected' : 'No Motion';
+    }
+    return ''; // Default label if sensor type is not recognized
 }
 
 // Call the fetchAndDrawGraph function on page load
